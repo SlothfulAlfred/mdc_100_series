d2bba0d20c7b45df2945b2c03d1832106e318966
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-05-31 20:09:04 -0400

    Implement MDC-104 section 6
    - Added animated menu
    - Refactored [OneProductColumn] and [TwoProductColumn] to avoid overflow errors when menu is open
    - Added [BuildContext] and [BoxContraints] parameters to [_buildStack]
    - Wrapped the body of [BackdropState] in a [LayourBuilder] to pass down parameters to [_buildStack]

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'model/product.dart';
 
+const double _kFlingVelocity = 2.0;
+
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
   final Widget frontLayer;
@@ -24,19 +26,60 @@ class Backdrop extends StatefulWidget {
 class _BackdropState extends State<Backdrop>
     with SingleTickerProviderStateMixin {
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
-  // TODO: Add AnimationController Widget
+  AnimationController _controller;
 
-  // TODO: Add BoxConstraints and BuildContext params to _buildStack
-  Widget _buildStack() {
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: RelativeRect.fromLTRB(0, 0, 0, 0),
+    ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: [
-        // TODO: Wrap backLayer in ExcludeSemantics Widget
-        widget.backLayer,
-        // TODO: Add a PositionedTransition
-        _FrontLayer(child: widget.frontLayer),
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
+        PositionedTransition(
+          child: _FrontLayer(child: widget.frontLayer),
+          rect: layerAnimation,
+        ),
       ],
     );
+  }
+
+  bool get _frontLayerVisible {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackLayerVisible() {
+    _controller.fling(
+      velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -45,9 +88,11 @@ class _BackdropState extends State<Backdrop>
       brightness: Brightness.light,
       elevation: 0.0,
       titleSpacing: 0.0,
-      // TODO: Replace Icon with IconButton
       // TODO: Replace title with _BackdropTitle parameter
-      leading: Icon(Icons.menu),
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: _toggleBackLayerVisible,
+      ),
       title: Text('SHRINE'),
       actions: [
         IconButton(
@@ -73,7 +118,7 @@ class _BackdropState extends State<Backdrop>
     );
     return Scaffold(
       appBar: appBar,
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
