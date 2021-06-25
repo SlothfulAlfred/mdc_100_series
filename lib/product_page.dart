a1db5de2cbe599ad83aa6892d0e5841ab485b3bb
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-06-25 00:04:26 -0400

    feat: add cart buttons and access
    Buttons to add items to the cart have been added in, they were built using the Provider package, so they use [context.watch] to rebuilt whenever necessary.
    
    The cart page can now be accessed from the cart [IconButton] in the app bar.

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import 'colors.dart';
 import 'supplemental/photo_hero.dart';
 import 'model/products_repository.dart';
+import 'model/cart_model.dart';
 import 'supplemental/loading_animation.dart';
 
 /// A page that displays information about, and animates, a single
@@ -13,16 +15,14 @@ import 'supplemental/loading_animation.dart';
 /// [ProductsRepository.allProducts].
 class ProductPage extends StatelessWidget {
   final int id;
-  Future item;
+  final Future item;
 
   ProductPage({
     @required this.id,
-  }) {
-    item = ProductsRepository.getById(id);
-  }
+  }) : item = ProductsRepository.getById(id);
 
   /// Creates the a customized [AppBar] widget for [ProductPage]
-  Widget _createAppbar() {
+  Widget _createAppbar(context) {
     return AppBar(
       // Looks very similar to the logo in the home page.
       title: Row(
@@ -38,8 +38,7 @@ class ProductPage extends StatelessWidget {
         IconButton(
           icon: Icon(Icons.shopping_cart),
           onPressed: () {
-            // TODO: Open cart page
-            print('cart page');
+            Navigator.pushNamed(context, '/cart');
           },
         )
       ],
@@ -57,7 +56,7 @@ class ProductPage extends StatelessWidget {
     );
   }
 
-  Widget _createMainPage(context, data) {
+  Widget _createMainPage(BuildContext context, data) {
     // size of the screen used as a [PhotoHero] parameter
     final sw = MediaQuery.of(context).size.width;
     // height of the screen used to size the text
@@ -67,8 +66,10 @@ class ProductPage extends StatelessWidget {
         decimalDigits: 0, locale: Localizations.localeOf(context).toString());
     // used to style the text (name, price of the product)
     final ThemeData theme = Theme.of(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
         SizedBox(height: 26),
         Padding(
@@ -82,12 +83,11 @@ class ProductPage extends StatelessWidget {
           ),
         ),
         SizedBox(height: 20),
-        // TODO: Add buttons to change quantity of items bought
         SizedBox(
           height: sh * 0.15,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
             children: [
               Text(
                 data.name,
@@ -96,11 +96,42 @@ class ProductPage extends StatelessWidget {
               ),
               Text(
                 formatter.format(data.price),
-                style: theme.textTheme.subtitle2,
+                style: theme.textTheme.headline6,
               ),
             ],
           ),
-        )
+        ),
+        _createButtons(context, data),
+      ],
+    );
+  }
+
+  Widget _createButtons(BuildContext context, var data) {
+    bool inCart = context
+        .select<CartModel, bool>((cart) => cart.products.containsKey(data));
+    var cart = context.watch<CartModel>();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          child: Icon(Icons.remove),
+          onPressed: inCart ? () => cart.remove(data) : null,
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(kShrineBrown900),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 32),
+          child: Text(inCart ? cart.products[data].toString() : '0'),
+        ),
+        ElevatedButton(
+          child: Icon(Icons.add),
+          onPressed: () => cart.add(data),
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(kShrineBrown900),
+          ),
+        ),
       ],
     );
   }
@@ -119,7 +150,7 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _createAppbar(),
+      appBar: _createAppbar(context),
       body: Container(
         color: kShrinePink100,
         // This [Material] Widget keeps the [ProductPage]
